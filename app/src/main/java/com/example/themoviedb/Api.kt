348deba8bb00b443cbda2c7798a38b25d824e91b
roberto <roberto93lazaro@gmail.com>
2022-08-29 23:30:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/themoviedb/Api.kt
@@ -0,0 +1,27 @@
+package com.example.themoviedb
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String = Constants.APIKEY,
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+        @Query("api_key") apiKey: String = Constants.APIKEY,
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(
+        @Query("api_key") apiKey: String = Constants.APIKEY,
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+}
---
+++ app/src/main/java/com/example/themoviedb/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.themoviedb
+
+class Constants {
+
+    companion object{
+        val APIKEY = "86cbedace2010b80127773f9553cc5b6"
+    }
+}
